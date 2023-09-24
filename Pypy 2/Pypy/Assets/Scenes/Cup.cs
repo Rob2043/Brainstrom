41478004f7fa7275633fd3ebd8cec04e171ae3fa
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2023-09-24 19:56:56 +0200

    Pypy.0.1

---
+++ Pypy 2/Pypy/Assets/Scenes/Cup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cup : MonoBehaviour
+{
+    [SerializeField] KeyCode keyOne;
+    [SerializeField] KeyCode keyTwo;
+    [SerializeField] UnityEngine.Vector3 moveDirection;
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(keyOne))
+        {
+            GetComponent<Rigidbody>().velocity += moveDirection;
+        }
+        if(Input.GetKey(keyTwo))
+        {
+            GetComponent<Rigidbody>().velocity -= moveDirection;
+        }
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
