fae93bbddaaf7c97561d78b7b9cf714769be8aa1
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2023-10-30 13:18:42 +0100

    Test

---
+++ Pypy/Assets/Scripts/LocalMunu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocalMunu : MonoBehaviour
+{
+    public void MenuMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void NextLevel()
+    {
+        int nextBuildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextBuildIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextBuildIndex);
+        }
+        else
+        {
+            // Если это последняя сцена в сборке, то можно загрузить меню или что-то другое.
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
