d60bc1f5ecb9e3d00c39db00e062161d7872a189
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2025-12-16 20:26:05 +0100

    Fix Level 12 & 13

--- Pypy/Assets/Scripts/Player/MovingBlocks.cs
+++ Pypy/Assets/Scripts/Player/MovingBlocks.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using CustomEventBus;
+using System.Collections;
 
 public class MovingBlocks : MonoBehaviour
 {
--- Pypy/Assets/Scripts/UI/BackgroundMoving.cs
+++ Pypy/Assets/Scripts/UI/BackgroundMoving.cs
@@ -1,13 +1,15 @@
 using Unity.VisualScripting;
 using UnityEngine;
-
+using System.Collections;
 
 public class BackgroundMoving : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Vector3 moveDirection;
+    [SerializeField, Range(0.1f, 1f)] private float _timeToCheck;
     private bool BackIsReady = false;
     private Rigidbody rb;
+    private int _countOfCollisions = 0;
 
     void Start()
     {
@@ -30,9 +32,23 @@ public class BackgroundMoving : MonoBehaviour
 
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
+        if (collision.gameObject.CompareTag("Wall") && _countOfCollisions < 1)
+        {
+            _countOfCollisions++;
+            BackIsReady = !BackIsReady;
+            StartCoroutine(CheckCollision());
+        }
+    }
+    private void OnCollisionExit(UnityEngine.Collision collision)
+    {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            BackIsReady = !BackIsReady;
+            _countOfCollisions = 0;
         }
+    }
+
+    IEnumerator CheckCollision()
+    {
+        yield return new WaitForSeconds(_timeToCheck);
     }
 }
